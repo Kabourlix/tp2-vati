a07b96ee6c1d6aa6fb9d75fbd34ecd7ff8373ecf
JuviaBerry <47364179+JuviaBerry@users.noreply.github.com>
2023-03-12 12:22:44 -0400

    Push annie superpixel

--- superpixel.py
+++ superpixel.py
@@ -1,3 +1,4 @@
+# imports
 from __future__ import print_function
 
 import PIL
@@ -12,19 +13,59 @@ from skimage.segmentation import felzenszwalb, slic, quickshift, clear_border
 from skimage.segmentation import mark_boundaries
 from skimage.util import img_as_float
 
+# Charger l'image
+def load_img(path):
+    return io.imread(path)
+
+
+# Mini traitement de l'image
+def process_img(img):
+    image = img.copy()
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray_image, (5, 5), 0)
+    (T, threshInv) = cv2.threshold(blurred, 50, 255, cv2.THRESH_BINARY)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
+    mask_image = cv2.morphologyEx(threshInv, cv2.MORPH_CLOSE, kernel)
+    res = cv2.bitwise_and(image, image, mask=mask_image)
+
+    return [gray_image,blurred,threshInv,mask_image,res]
+
+# Appliquer la segmentation par la méthode Felzenszwalb
+# res : image de base avec mask appliqué
+def seg_felzenszwalb(res):
+    return felzenszwalb(res, scale=1, sigma=0.5, min_size=600)
+    # TODO : image avec mask utilisé pour ne pas segmenter le fond noir
+    # TODO : diminution scale pour la taille des segmentations
+    # TODO : augmentation min size pour ne pas prendre en compte des petits points de couleur au sein d'une pierre
+
+# Appliquer la segmentation par la méthode SLIC
+# loaded_img : image de base
+# mask_image : mask de l'image ( image binaire )
+def seg_slic(loaded_img, mask_image) :
+    # convertir en float pr utiliser skimage
+    img = img_as_float(loaded_img)
+    return slic(img, n_segments=250, compactness=10, sigma=1, mask=mask_image)
+    # TODO : mask ajouté pour enlever les segmentation du fond noir
+
+# Appliquer la segmentation par la méthode QuickShift
+# loaded_img : image de base
+def seg_quickshift(loaded_img) :
+    # convertir en float pr utiliser skimage
+    img = img_as_float(loaded_img)
+    return quickshift(img, kernel_size=5, max_dist=100, ratio=0.5)
+    # TODO : Pas de param mask, on ne peut pas utiliser l'image masqué non plus....fond noir segmenté !
+    # TODO : max_dist augmenté pour siminué le nombre de cluster
+
+
+'''MAIN SCRIPT'''
+
 # image original
-loaded_img = io.imread('C:/Users/annie/PycharmProjects/pythonProject/Echantillion1Mod2_301.png')
+loaded_img = load_img('C:/Users/annie/PycharmProjects/pythonProject/Echantillion1Mod2_301.png')
 
-# grayscale
-image = loaded_img.copy()
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(gray_image, (5, 5), 0)
-(T, threshInv) = cv2.threshold(blurred, 50, 255, cv2.THRESH_BINARY)
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
-mask_image = cv2.morphologyEx(threshInv, cv2.MORPH_CLOSE, kernel)
-res = cv2.bitwise_and(image, image, mask=mask_image)
+# traitement image
+gray_image,blurred,threshInv,mask_image,res = process_img(loaded_img)
 
-# Afficher rapidement les images
+# Affichage rapide de la procédure de traitement image
 plt.figure()
 plt.subplot(231)
 plt.imshow(loaded_img)
@@ -45,31 +86,18 @@ plt.subplot(236)
 plt.imshow(res)
 plt.title('Image with mask')
 
-# TODO : pour le rapport : montrer les résultats sans le mask
-
-# convertir en float pr utiliser skimage
-img = img_as_float(loaded_img)
-
-# felzenszwalb
-segments_fz = felzenszwalb(res, scale=1, sigma=0.5, min_size=600)
-# TODO : image avec mask utilisé pour ne pas segmenter le fond noir
-# TODO : diminution scale pour la taille des segmentations
-# TODO : augmentation min size pour ne pas prendre en compte des petits points de couleur au sein d'une pierre
-
-# slic
-segments_slic = slic(img, n_segments=250, compactness=10, sigma=1, mask=mask_image)
-# TODO : mask ajouté pour enlever les segmentation du fond noir
-
-# quickShift
-segments_quick = quickshift(img, kernel_size=5, max_dist=100, ratio=0.5)
-# TODO : Pas de param mask, on ne peut pas utiliser l'image masqué non plus....fond noir segmenté !
-# TODO : max_dist augmenté pour siminué le nombre de cluster
+# Appeler les méthodes de segmentation
+segments_fz = seg_felzenszwalb(res)
+segments_slic = seg_slic(loaded_img, mask_image)
+segments_quick = seg_quickshift(loaded_img)
 
 print("Felzenszwalb's number of segments: %d" % len(np.unique(segments_fz)))
 print("Slic number of segments: %d" % len(np.unique(segments_slic)))
 print("Quickshift number of segments: %d" % len(np.unique(segments_quick)))
 
-# Afficher les trois segmentations
+# convertir en float pr utiliser skimage
+img = img_as_float(loaded_img)
+# Afficher les segmentations ( 1 - avant traitement image et edit param, 2 - après traitement image et edit param )
 plt.figure()
 plt.subplot(231)
 plt.imshow(mark_boundaries(img, felzenszwalb(img, scale=100, sigma=0.5, min_size=100)))
@@ -92,11 +120,11 @@ plt.title("Quickshift 2")
 
 # Segmentation en plusieurs fichiers
 # TODO : On utilisera seulement ceux de SLIC car c'est le meilleur
-
 plt.figure()
 plt.imshow(label2rgb(segments_slic,
-                     image,
-                     kind = 'avg'))
-plt.title('Segment label')
+                     loaded_img,
+                     kind='avg'))
+plt.title('Segment label (SLIC)')
+
 plt.show()
 
